5e0afaa8d2b6c4b1c75b8622e2e1f083e530bd34
Jori Peterson <joripeterson@gmail.com>
2019-05-15 10:19:37 -0600

    Add average testing

--- test/building.rb
+++ test/building.rb
@@ -4,9 +4,11 @@ require './lib/apartment'
 require './lib/renter'
 require './lib/building'
 
-class ApartmentTest < Minitest::Test
+class BuildingTest < Minitest::Test
 
   def setup
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @a2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     @building = Building.new
   end
 
@@ -16,5 +18,17 @@ class ApartmentTest < Minitest::Test
 
   def test_building_has_attributes
     assert [], @building.units
-  end    
+  end
+
+  def test_building_can_add_units
+    @building.add_unit(@a1)
+    @building.add_unit(@a2)
+    assert_equal [@a1, @a2], @building.units
+  end
+
+  def test_building_can_find_average_rent
+    @building.add_unit(@a1)
+    @building.add_unit(@a2)
+    assert_equal 1099.5, @building.average_rent
+  end
 end
