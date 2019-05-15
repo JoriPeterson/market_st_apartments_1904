e76d1b7b9e4c7e772617393ad9d947ece6a96bf5
Jori Peterson <joripeterson@gmail.com>
2019-05-15 09:25:33 -0600

    Add renter method and local variable

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,11 +1,16 @@
 class Apartment
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
 
   def initialize(details)
     @number = details[:number]
     @monthly_rent = details[:monthly_rent]
     @bathrooms = details[:bathrooms]
     @bedrooms = details[:bedrooms]
+    @renter = renter
+  end
+
+  def add_renter(renter)
+    @renter = renter
   end
 
 end
