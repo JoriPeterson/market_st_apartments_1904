c5400061e3c4656f82d1390b0597288176a67b92
Jori Peterson <joripeterson@gmail.com>
2019-05-15 11:03:58 -0600

    In process of highest rent method

--- lib/building.rb
+++ lib/building.rb
@@ -10,10 +10,21 @@ class Building
   end
 
   def average_rent
-    ave = @units.map do |apartment|
+    rents = @units.map do |apartment|
       apartment.monthly_rent
     end
-    ave.sum.to_f / ave.length
+    ave = rents.sum.to_f / rents.length
+  end
+
+  def renter_with_highest_rent
+    highest = @units.max_by do |apartment|
+      apartment.monthly_rent
+
+# require 'pry'; binding.pry
+      highest.map do |renter|
+        highest.renter
+      end
+    end
   end
 
 end
--- test/building.rb
+++ test/building.rb
@@ -7,6 +7,8 @@ require './lib/building'
 class BuildingTest < Minitest::Test
 
   def setup
+    @jessie = Renter.new("Jessie")
+    @spencer = Renter.new("Spencer")
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @a2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     @building = Building.new
@@ -31,4 +33,14 @@ class BuildingTest < Minitest::Test
     @building.add_unit(@a2)
     assert_equal 1099.5, @building.average_rent
   end
+
+  def test_for_building_with_highest_rent
+    @a2.add_renter(@spenser)
+    @building.add_unit(@a1)
+    @building.add_unit(@a2)
+    assert_equal @spenser, @building.renter_with_highest_rent
+    # @a1.add_renter(@jessie)
+    # assert_equal @jessie, @building.renter_with_highest_rent
+  end
+
 end
